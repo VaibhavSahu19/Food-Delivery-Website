eb2da5be33f1323afd0e963e67546f4eea901683
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-03-27 10:34:39 +0530

    changed heading color and added location in restraunts

--- frontend/src/components/restuarantCard/RestuarantCard.js
+++ frontend/src/components/restuarantCard/RestuarantCard.js
@@ -2,7 +2,8 @@ import { CDN_URL } from "../../utils/constants";
 import { FaStar } from "react-icons/fa";
 const RestuarantCard = (props) => {
   const { resData } = props;
-  const { name, cuisines, avgRating, costForTwo, sla } = resData?.info; //optional chaining
+  const { name, cuisines, avgRating, costForTwo, areaName, sla, locality } = resData?.info;
+  console.log(resData); //optional chaining
   return (
     <div className="grid grid-flow-col gap-4 sm:grid sm:gap-3 cursor-pointer items-center sm:justify-stretch sm:grid-flow-row transition-transform ease-in-out transform hover:scale-95">
       <div className=" w-full h-full relative overflow-hidden drop-shadow-lg">
@@ -13,14 +14,14 @@ const RestuarantCard = (props) => {
         />
       </div>
       <div className="overflow-x-auto sm:ml-3 sm:overflow-y-auto">
-        <h1 className=" text-lg font-bold text-gray-900 overflow-hidden w-full text-ellipsis whitespace-nowrap  break-words">
+        <h1 className=" text-lg font-bold overflow-hidden w-full text-ellipsis whitespace-nowrap  break-words">
           {name}
         </h1>
         <h2 className="grid grid-flow-col items-center mt-[2px] gap-[2px] justify-start">
           <p className="text-green-600">
             <FaStar />
           </p>
-          <p className="text-lg font-bold text-gray-900 overflow-hidden w-full break-words">
+          <p className="text-lg font-bold overflow-hidden w-full break-words">
             <span>{avgRating} •</span> {sla?.slaString}
           </p>
         </h2>
@@ -29,7 +30,7 @@ const RestuarantCard = (props) => {
             {cuisines.join(", ")}
           </p>
           <p className="text-base font-light overflow-hidden w-full text-ellipsis whitespace-nowrap break-words">
-            Location
+            {locality}, {areaName} 
           </p>
         </h3>
       </div>
